d5ffa415c967159dc2dd681cab5b6888d7089b15
ezratweaver <ezratweaver@gmail.com>
2024-06-02 16:04:35 -0400

    Setup initial fastify instance

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = Number.parseInt(process.env.API_PORT ?? '8080')
+const host = process.env.API_HOST ?? '0.0.0.0'
+
+const server = Fastify()
+
+const main = async () => {
+  try {
+    await server.listen({ port, host })
+    console.log('Server ready at http://localhost:3000')
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+void main()
