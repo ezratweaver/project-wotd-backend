2e34d70da7e831275f50d626f3e374f38f3cba0d
ezratweaver <ezratweaver@gmail.com>
2024-06-29 18:36:10 -0400

    Add basic signup route stuff

--- src/routes/auth/signUp.ts
+++ src/routes/auth/signUp.ts
@@ -1,13 +1,32 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 import prisma from "../database";
 
 const url = "/signup";
 const method = "POST";
 
-const handler = async (request: FastifyRequest, reply: FastifyReply) => {};
+const signUpRequestBody = z.object({
+  email: z.string().email(),
+  firstName: z.string(),
+  password: z.string(),
+});
+
+type SignUpRequestBody = z.infer<typeof signUpRequestBody>;
+
+const handler = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { email, firstName, password } = request.body as SignUpRequestBody;
+
+  prisma.user.create({
+    data: {
+      email,
+      firstName,
+      password,
+    },
+  });
+};
 
 const signUp = async (fastify: FastifyInstance) => {
-  fastify.route({
+  fastify.route<{ Body: SignUpRequestBody }>({
     method,
     handler,
     url,
