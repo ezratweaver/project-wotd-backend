7b272240d34cf2586545d8bca332a676241f7454
ezratweaver <ezratweaver@gmail.com>
2024-06-30 16:30:49 -0400

    Add logic to signup route

--- src/routes/auth/signUp.ts
+++ src/routes/auth/signUp.ts
@@ -1,9 +1,7 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { $ref } from "../../app";
-
-const url = "/signup";
-const method = "POST";
+import prisma from "../../database";
 
 export const signUpRequestBody = z.object({
   email: z.string().email(),
@@ -13,28 +11,36 @@ export const signUpRequestBody = z.object({
 
 type SignUpRequestBody = z.infer<typeof signUpRequestBody>;
 
+const url = "/signup";
+const method = "POST";
+const schema = {
+  response: {
+    201: {},
+  },
+};
+
 const handler = async (request: FastifyRequest, reply: FastifyReply) => {
   const { email, firstName, password } = request.body as SignUpRequestBody;
 
-  // prisma.user.create({
-  //   data: {
-  //     email,
-  //     firstName,
-  //     password,
-  //   },
-  // });
+  prisma.user.create({
+    data: {
+      email,
+      firstName,
+      password,
+    },
+  });
 
-  console.log("signed up!");
+  return reply
+    .status(201)
+    .send({ result: "success", message: "New user was created successfully" });
 };
 
 const signUp = async (fastify: FastifyInstance) => {
   fastify.route<{ Body: SignUpRequestBody }>({
     method,
     schema: {
+      ...schema,
       body: $ref("signUpRequestBody"),
-      response: {
-        201: {},
-      },
     },
     handler,
     url,
