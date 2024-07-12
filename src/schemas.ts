e578cb0f70ff53c57e4daeede384389398b6b825
ezratweaver <ezratweaver@gmail.com>
2024-07-11 21:18:16 -0400

    Remove failing schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import { SignUpRequestBody } from "./schemas/SignUpRequestBody";
 import { GenericResponse } from "./schemas/GenericResponse";
 import { LoginRequestBody } from "./schemas/LoginRequestBody";
-import { FetchWOTDRequestBody } from "./schemas/FetchWOTDRequestBody";
 import { FetchWOTDResponse } from "./schemas/FetchWOTDResponse";
 
 const schemasDir = resolve(__dirname, "schemas");
@@ -12,13 +11,12 @@ const schemas = {
   SignUpRequestBody,
   GenericResponse,
   LoginRequestBody,
-  FetchWOTDRequestBody,
   FetchWOTDResponse,
 };
 
-if (readdirSync(schemasDir).length > Object.values(schemas).length) {
+if (readdirSync(schemasDir).length !== Object.values(schemas).length) {
   throw new Error(
-    "More schemas in src/schemas then imported into schemas file, did you forgot to import a schema?",
+    "Schemas imported do not match schemas in src/schemas directory, did you forgot to import a schema?",
   );
 }
 
--- src/schemas/FetchWOTDRequestBody.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const FetchWOTDRequestBody = z.object({
-  date: z.date(),
-});
-
-type FetchWOTDRequestBodyType = z.infer<typeof FetchWOTDRequestBody>;
-
-export default FetchWOTDRequestBodyType;
