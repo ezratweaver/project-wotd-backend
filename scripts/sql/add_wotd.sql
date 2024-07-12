fc5ea0310366a7caa3fadf8106f0f055b23a1634
ezratweaver <ezratweaver@gmail.com>
2024-07-11 22:32:28 -0400

    Add sql script for inserting wotd

---
+++ scripts/sql/add_wotd.sql
@@ -0,0 +1,15 @@
+INSERT INTO "WordOfTheDay"(
+  word,
+  definition,
+  pronunciation,
+  "partOfSpeech",
+  "usage",
+  "date"
+)VALUES(
+'your_word',
+'your_definition',
+'your_pronunciation',
+'your_partOfSpeech',
+'your_usage',
+'2020-01-01T00:00:00Z'
+);
