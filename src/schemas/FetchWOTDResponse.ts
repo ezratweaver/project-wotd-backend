b7226f02f69f4eb81436f5258d8855c12a4009f8
ezratweaver <ezratweaver@gmail.com>
2024-07-13 21:30:22 -0400

    Fix spelling error in schema

--- src/schemas/FetchWOTDResponse.ts
+++ src/schemas/FetchWOTDResponse.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const FetchWOTDResponse = z.object({
   word: z.string(),
   definition: z.string(),
-  pronuciation: z.string(),
+  pronunciation: z.string(),
   partOfSpeech: z.string(),
   usage: z.string(),
   date: z.date(),
