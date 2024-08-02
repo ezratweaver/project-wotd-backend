8b2b39f6277c0f4121d8d1e4c5f368e8605e5ef7
ezratweaver <ezratweaver@gmail.com>
2024-08-02 10:44:08 -0400

    Change datehelpers to UTC

--- src/helper/dateHelpers.ts
+++ src/helper/dateHelpers.ts
@@ -1,6 +1,12 @@
-export const getDateWithOffset = (offset: number): Date => {
-  const currentDate = new Date();
+export const dateWithOffset = (offset: number, date?: Date | string): Date => {
+  const currentDate = date ? new Date(date) : new Date();
   const offsetDate = new Date(currentDate);
   offsetDate.setDate(currentDate.getDate() + offset);
   return offsetDate;
+};
+
+export const dateWithoutHours = (date: Date | string) => {
+  const newDate = new Date(date);
+  newDate.setUTCHours(0, 0, 0, 0);
+  return newDate;
 };
