e36e6c34c3e1183645288aeb0c92dd20310eed0c
ezratweaver <ezratweaver@gmail.com>
2024-08-09 14:13:06 -0400

    Modulate Word type

--- src/schemas/FetchWOTDResponse.ts
+++ src/schemas/FetchWOTDResponse.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
+export const Word = z.object({
+  word: z.string(),
+  definition: z.string(),
+  pronunciation: z.string(),
+  partOfSpeech: z.string(),
+  usage: z.string(),
+  date: z.date(),
+});
+
 export const FetchWOTDResponse = z.object({
-  wordData: z
-    .object({
-      word: z.string(),
-      definition: z.string(),
-      pronunciation: z.string(),
-      partOfSpeech: z.string(),
-      usage: z.string(),
-      date: z.date(),
-    })
-    .optional(),
+  wordData: Word.optional(),
   wordNextDay: z.boolean(),
   wordPrevDay: z.boolean(),
 });
