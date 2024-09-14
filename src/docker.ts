49699bdeba3440d2e683a0a7117fdb5f3dce2c4d
ezratweaver <ezratweaver@gmail.com>
2024-09-13 23:59:07 -0400

    Add smtp4dev docker on start

--- src/docker.ts
+++ src/docker.ts
@@ -3,12 +3,20 @@ import { execa } from "execa";
 const DB_DOCKER_IMAGE_NAME = process.env.DB_DOCKER_IMAGE_NAME;
 const DB_PORT = process.env.DB_PORT;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const SMTP4DEV_PORT = process.env.SMTP4DEV_PORT;
+const SMTP4DEV_NAME = process.env.SMTP4DEV_NAME;
 
 const $ = execa({ verbose: "short" });
 
-if (!DB_DOCKER_IMAGE_NAME || !DB_PORT || !DB_PASSWORD) {
+if (
+  !DB_DOCKER_IMAGE_NAME ||
+  !DB_PORT ||
+  !DB_PASSWORD ||
+  !SMTP4DEV_PORT ||
+  !SMTP4DEV_NAME
+) {
   throw new Error(
-    "Must have a DB_DOCKER_IMAGE_NAME, DB_PORT, and DB_PASSWORD in the '.env' file.",
+    "Must have a DB_DOCKER_IMAGE_NAME, DB_PORT, DB_PASSWORD, SMTP4DEV_PORT, and SMTP4DEV_NAME in the '.env' file.",
   );
 }
 
@@ -36,10 +44,17 @@ export const buildPostgresContainer = async () => {
 };
 
 export const startDockerContainers = async () => {
+  // Start postgres
   if (!(await containerIsRunning(DB_DOCKER_IMAGE_NAME))) {
     if (!(await containerExists(DB_DOCKER_IMAGE_NAME))) {
       await buildPostgresContainer();
     }
     await $`sudo docker start /${DB_DOCKER_IMAGE_NAME}`;
   }
+
+  // Start smtp4dev
+  if (!(await containerIsRunning(SMTP4DEV_NAME)))
+    await $`sudo docker run -d --name ${SMTP4DEV_NAME} -p ${SMTP4DEV_PORT}:${SMTP4DEV_PORT} -p 2525:25 rnwood/smtp4dev`;
+
+  console.log(`SMTP4Dev ready at http://localhost:${SMTP4DEV_PORT}`);
 };
