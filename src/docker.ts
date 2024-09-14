10bdffce08e4e8f0c639bb97406ff56d29cc6c7a
ezratweaver <ezratweaver@gmail.com>
2024-09-14 00:18:17 -0400

    Add SMTP4DEV smtp port env variable

--- src/docker.ts
+++ src/docker.ts
@@ -4,6 +4,7 @@ const DB_DOCKER_IMAGE_NAME = process.env.DB_DOCKER_IMAGE_NAME;
 const DB_PORT = process.env.DB_PORT;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const SMTP4DEV_WEB_PORT = process.env.SMTP4DEV_WEB_PORT;
+const SMTP4DEV_SMTP_PORT = process.env.SMTP4DEV_SMTP_PORT;
 const SMTP4DEV_NAME = process.env.SMTP4DEV_NAME;
 
 const $ = execa({ verbose: "short" });
@@ -13,10 +14,11 @@ if (
   !DB_PORT ||
   !DB_PASSWORD ||
   !SMTP4DEV_WEB_PORT ||
-  !SMTP4DEV_NAME
+  !SMTP4DEV_NAME ||
+  !SMTP4DEV_SMTP_PORT
 ) {
   throw new Error(
-    "Must have a DB_DOCKER_IMAGE_NAME, DB_PORT, DB_PASSWORD, SMTP4DEV_WEB_PORT, and SMTP4DEV_NAME in the '.env' file.",
+    "Must have a DB_DOCKER_IMAGE_NAME, DB_PORT, DB_PASSWORD, SMTP4DEV_WEB_PORT, SMTP4DEV_NAME, SMTP4DEV_SMTP_PORT in the '.env' file.",
   );
 }
 
@@ -54,7 +56,7 @@ export const startDockerContainers = async () => {
 
   // Start smtp4dev
   if (!(await containerIsRunning(SMTP4DEV_NAME)))
-    await $`sudo docker run -d --name ${SMTP4DEV_NAME} -p ${SMTP4DEV_WEB_PORT}:${SMTP4DEV_WEB_PORT} -p 2525:25 rnwood/smtp4dev`;
+    await $`sudo docker run -d --name ${SMTP4DEV_NAME} -p ${SMTP4DEV_WEB_PORT}:${SMTP4DEV_WEB_PORT} -p ${SMTP4DEV_SMTP_PORT}:${SMTP4DEV_SMTP_PORT} rnwood/smtp4dev`;
 
   console.log(`SMTP4Dev ready at http://localhost:${SMTP4DEV_WEB_PORT}`);
 };
