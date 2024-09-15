7d148aec234a30f6b90f94bb2cbdcb05ac84a3f9
ezratweaver <ezratweaver@gmail.com>
2024-09-14 20:26:23 -0400

    Add otp generator

---
+++ src/helper/otpHelpers.ts
@@ -0,0 +1,13 @@
+export function generateOTP(length: number) {
+  const allowedCharacters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+";
+
+  let otp = "";
+  for (let i = 1; i <= length; i++) {
+    const index = Math.floor(Math.random() * allowedCharacters.length);
+
+    otp = otp + allowedCharacters[index];
+  }
+
+  return otp;
+}
