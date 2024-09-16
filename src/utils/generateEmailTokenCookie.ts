b4e3b5845039a2eea065af64e1f7710d30c7ca4b
ezratweaver <ezratweaver@gmail.com>
2024-09-15 21:20:46 -0400

    Add util for generating email token cookie

---
+++ src/utils/generateEmailTokenCookie.ts
@@ -0,0 +1,28 @@
+import { FastifyReply } from "fastify";
+import { generateOTP } from "../helper/otpHelpers";
+import { dateWithOffset } from "../helper/dateHelpers";
+
+export interface EmailTokenType {
+  token: string;
+  email: string;
+  expires: Date;
+}
+
+export const genereateEmailTokenCookie = async (
+  email: string,
+  reply: FastifyReply,
+) => {
+  const otp = generateOTP(16);
+
+  const tokenToSign: EmailTokenType = {
+    token: otp,
+    email,
+    expires: dateWithOffset(1),
+  };
+
+  const jwtEmailToken = await reply.jwtSign(tokenToSign);
+
+  reply.setCookie("email", jwtEmailToken);
+
+  return otp;
+};
