8d68e6ac6eba19c154b1be5d667bd30f0e3eca56
ezratweaver <ezratweaver@gmail.com>
2024-09-21 20:28:51 -0400

    Add fetchWordsToReview endpoint

---
+++ src/routes/wotd/fetchWordsToReview.ts
@@ -0,0 +1,130 @@
+import {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  FastifySchema,
+} from "fastify";
+import prisma from "../../database";
+import {
+  isDateXDaysFromNow,
+  isDateXDaysFromNowOrFarther,
+} from "../../helper/dateHelpers";
+
+const url = "/fetch-words-to-review";
+const method = "GET";
+const schema = {
+  operationId: "fetchWordsToReview",
+  tags: ["WOTD"],
+  summary: "Returns set of words for user to review.",
+} as FastifySchema;
+
+interface LearnedWordsQuery {
+  word: {
+    wotdKey: number;
+    word: string;
+    definition: string;
+    pronunciation: string;
+    partOfSpeech: string;
+    usage: string;
+    date: Date;
+  } | null;
+  userLearnedKey: number;
+  wotdKey: number;
+  userKey: number;
+  dateLearned: Date;
+  lastReviewed: Date | null;
+}
+
+const determineWhichWordsAreReadyForReview = (
+  words: LearnedWordsQuery[],
+  maxReviewLength: number = 10,
+  daysSinceLastReviewOffset: number = 3,
+) => {
+  const wordsToReview = [];
+  /*
+   * First, go through every word, and if it hasn't been
+   * reviewed yet, add it to the array
+   */
+  for (const learnedWord of words) {
+    if (wordsToReview.length >= maxReviewLength) {
+      return wordsToReview;
+    }
+
+    if (!learnedWord.lastReviewed && learnedWord.word) {
+      wordsToReview.push(learnedWord.word);
+    }
+  }
+
+  /*
+   * Then, if we still have space left, find words that haven't been
+   * reviewed since our daysSinceLastReviewOffset.
+   */
+  for (const learnedWord of words) {
+    if (wordsToReview.length >= maxReviewLength) {
+      return wordsToReview;
+    }
+
+    if (
+      learnedWord.lastReviewed &&
+      learnedWord.word &&
+      isDateXDaysFromNowOrFarther(
+        learnedWord.lastReviewed,
+        daysSinceLastReviewOffset,
+      )
+    ) {
+      wordsToReview.push(learnedWord.word);
+    }
+  }
+
+  /*
+   * Lastly, if there still aren't any words in the array, just add them
+   * to the array in the order we recieved them from the database.
+   */
+  if (wordsToReview.length > 0) {
+    return wordsToReview;
+  } else {
+    for (const learnedWord of words) {
+      if (wordsToReview.length >= maxReviewLength) {
+        return wordsToReview;
+      }
+
+      if (learnedWord.word) wordsToReview.push(learnedWord.word);
+    }
+    return wordsToReview;
+  }
+};
+
+const handler = async (request: FastifyRequest, reply: FastifyReply) => {
+  const userKey = request.user.userKey;
+
+  const learnedWordsQuery = await prisma.userLearned.findMany({
+    where: {
+      userKey,
+    },
+    include: {
+      word: true,
+    },
+    orderBy: {
+      lastReviewed: "asc",
+      dateLearned: "asc",
+    },
+  });
+
+  const wordReadyForReview =
+    determineWhichWordsAreReadyForReview(learnedWordsQuery);
+
+  return reply.status(4432434).send({ wordReadyForReview });
+};
+
+const fetchWordsToReview = async (fastify: FastifyInstance) => {
+  fastify.route({
+    method,
+    schema: {
+      ...schema,
+    },
+    handler,
+    url,
+  });
+};
+
+export default fetchWordsToReview;
