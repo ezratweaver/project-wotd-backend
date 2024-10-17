eb8b07f0d4f15140029566ef6ea1da3d96c1f1bf
Ezra Weaver <ezratweaver@gmail.com>
2024-10-17 17:51:14 -0400

    Add addWOTD logic

--- src/routes/admin/addWOTD.ts
+++ src/routes/admin/addWOTD.ts
@@ -4,6 +4,9 @@ import {
   FastifyRequest,
   FastifySchema,
 } from "fastify";
+import AddWOTDRequestBodyType from "../../schemas/AddWOTDRequestBody";
+import { compareSync } from "bcrypt";
+import prisma from "../../database";
 
 const url = "/add-wotd";
 const method = "POST";
@@ -14,10 +17,33 @@ const schema = {
     "Add a word of the day to be put into the database, for users to learn and review.",
 } as FastifySchema;
 
-const handler = async (request: FastifyRequest, reply: FastifyReply) => {};
+const handler = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { password, word } = request.body as AddWOTDRequestBodyType;
+
+  if (!compareSync(password, process.env.ADMIN_PASSWORD ?? "")) {
+    console.warn(
+      "/add-wotd authentication failed, did you forget your password?",
+    );
+    return reply.status(401).send();
+  }
+
+  let pronunciationSoundBuffer: Buffer;
+  if (word.pronunciationSound) {
+    const arrayBuffer = await word.pronunciationSound.arrayBuffer();
+
+    pronunciationSoundBuffer = Buffer.from(arrayBuffer);
+  }
+
+  await prisma.wordOfTheDay.create({
+    data: {
+      ...word,
+      pronunciationSound: {} as Buffer,
+    },
+  });
+};
 
 const addWOTD = async (fastify: FastifyInstance) => {
-  fastify.route<{ Body: {} }>({
+  fastify.route<{ Body: AddWOTDRequestBodyType }>({
     method,
     schema: {
       ...schema,
