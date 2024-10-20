19cd626f4d6993d661b9c5c7ac2f5dd137a95005
Ezra Weaver <ezratweaver@gmail.com>
2024-10-20 19:41:31 -0400

    Remove pronunciation sould field

---
+++ prisma/migrations/20241020233821_remove_pronunciation_sound_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `pronunciationSound` on the `WordOfTheDay` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "WordOfTheDay" DROP COLUMN "pronunciationSound";
