1bda09ad993b96f3aa378e9ad767b9d2c6ec6456
Ezra Weaver <ezratweaver@gmail.com>
2024-10-20 22:26:34 -0400

    Implement s3 integration with polly when adding wotd

--- src/routes/admin/addWOTD.ts
+++ src/routes/admin/addWOTD.ts
@@ -7,7 +7,10 @@ import {
 import AddWOTDRequestBodyType from "../../schemas/AddWOTDRequestBody";
 import { compareSync } from "bcrypt";
 import prisma from "../../database";
-import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+import {
+  PollyClient,
+  StartSpeechSynthesisTaskCommand,
+} from "@aws-sdk/client-polly";
 import { $ref } from "../../app";
 
 const url = "/add-wotd";
@@ -21,10 +24,10 @@ const schema = {
 
 const handler = async (request: FastifyRequest, reply: FastifyReply) => {
   const pollyClient = new PollyClient({
-    region: "us-east-1",
+    region: process.env.AWS_REGION,
     credentials: {
-      accessKeyId: process.env.POLLY_ACCESS_KEY ?? "",
-      secretAccessKey: process.env.POLLY_SECRET_KEY ?? "",
+      accessKeyId: process.env.AWS_ACCESS_KEY ?? "",
+      secretAccessKey: process.env.AWS_SECRET_KEY ?? "",
     },
   });
 
@@ -37,46 +40,39 @@ const handler = async (request: FastifyRequest, reply: FastifyReply) => {
     return reply.status(401).send();
   }
 
-  let pronunciationSoundBuffer: Buffer | undefined;
-  if (word.pronunciationSound) {
-    const arrayBuffer = await word.pronunciationSound.arrayBuffer();
-
-    pronunciationSoundBuffer = Buffer.from(arrayBuffer);
-  } else {
-    const voiceCommand = new SynthesizeSpeechCommand({
+  try {
+    const voiceCommand = new StartSpeechSynthesisTaskCommand({
       OutputFormat: "mp3",
+      OutputS3BucketName: process.env.AWS_BUCKET_NAME,
+      OutputS3KeyPrefix: word.word,
       Text: word.word,
       VoiceId: "Amy",
     });
 
-    try {
-      const response = await pollyClient.send(voiceCommand);
+    const response = await pollyClient.send(voiceCommand);
 
-      const soundByteArray = await response.AudioStream?.transformToByteArray();
+    if (!response.SynthesisTask)
+      throw new Error("No SynthesisTask was returned.");
 
-      if (!soundByteArray) {
-        throw new Error("Failed to get sound byte array from AWS.");
-      }
-
-      pronunciationSoundBuffer = Buffer.from(soundByteArray);
-    } catch (error) {
-      console.log({
-        message: "Failed to get pronunciation sound from AWS Polly.",
-        error,
-      });
-      return reply.status(500).send({
-        error: "AWS Polly Failure",
-        messge: "Failed to fetch pronunciation sound from AWS Polly.",
-      });
-    }
+    if (!response.SynthesisTask) throw new Error("No output URI was returned.");
+  } catch (error) {
+    console.log({
+      message: "Failed to create pronunciation sound and put it into S3.",
+      error,
+    });
+    return reply.status(500).send({
+      error: "AWS Polly Failure",
+      messge: "Failed to create pronunciation sound.",
+    });
   }
 
   await prisma.wordOfTheDay.create({
     data: {
       ...word,
-      pronunciationSound: pronunciationSoundBuffer,
     },
   });
+
+  return reply.status(201).send();
 };
 
 const addWOTD = async (fastify: FastifyInstance) => {
