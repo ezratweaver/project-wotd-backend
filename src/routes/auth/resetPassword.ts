96e5d77cbe15fd1e976f176ae71046f9c4875bc7
Ezra Weaver <ezratweaver@gmail.com>
2024-10-24 23:21:43 -0400

    Add reset password route

---
+++ src/routes/auth/resetPassword.ts
@@ -0,0 +1,89 @@
+import {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  FastifySchema,
+} from "fastify";
+import ResetPasswordRequestBodyType from "../../schemas/ResetPasswordRequestBody";
+import { $ref } from "../../app";
+import { randomBytes } from "crypto";
+import { hashSync } from "bcrypt";
+import prisma from "../../database";
+import setAuthenticationCookie from "../../utils/setAuthenticationCookie";
+
+const url = "/reset-password";
+const method = "POST";
+const schema = {
+  operationId: "resetPassword",
+  tags: ["Authentication"],
+  summary:
+    "Given a valid resetPassword jwt cookie, allows reseting user password from email in cookie.",
+} as FastifySchema;
+
+const invalidResetPasswordCookie = {
+  error: "Invalid Reset Password Cookie",
+  message: "The reset password cookie provided is invalid.",
+};
+
+const handler = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { password } = request.body as ResetPasswordRequestBodyType;
+
+  const resetPasswordCookie = request.cookies["resetPassword"];
+
+  if (!resetPasswordCookie) {
+    return reply.status(401).send(invalidResetPasswordCookie);
+  }
+
+  const unsignedResetPasswordCookie =
+    request.unsignCookie(resetPasswordCookie).value;
+
+  if (!unsignedResetPasswordCookie) {
+    return reply.status(401).send(invalidResetPasswordCookie);
+  }
+
+  let verifiedCookie: { allowedToReset: boolean; email: string };
+  try {
+    verifiedCookie = await request.decodeToken(unsignedResetPasswordCookie);
+  } catch (err) {
+    return reply.status(401).send(invalidResetPasswordCookie);
+  }
+
+  if (!verifiedCookie.allowedToReset) {
+    return reply.status(401).send(invalidResetPasswordCookie);
+  }
+
+  const salt = randomBytes(16).toString("hex");
+  const hashedPassword = hashSync(password + salt, 10);
+
+  const userFromDb = await prisma.user.update({
+    data: {
+      password: hashedPassword,
+      salt,
+    },
+    where: {
+      email: verifiedCookie.email,
+    },
+  });
+
+  await setAuthenticationCookie({
+    userKey: userFromDb.userKey,
+    email: userFromDb.email,
+    reply,
+  });
+
+  return reply.status(200).send();
+};
+
+const resetPassword = async (fastify: FastifyInstance) => {
+  fastify.route<{ Body: ResetPasswordRequestBodyType }>({
+    method,
+    schema: {
+      ...schema,
+      body: $ref("ResetPasswordRequestBody"),
+    },
+    handler,
+    url,
+  });
+};
+
+export default resetPassword;
